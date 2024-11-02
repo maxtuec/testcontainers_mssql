13a20f34e32b0671302ae242c424ea840fd4ecbc
maxtuec <maxtuec@gmail.com>
2024-11-02 16:29:39 +0100

    corrected composite foreign key in stoplog

--- NextStop/NextStop.Dal.Common/StopLog.cs
+++ NextStop/NextStop.Dal.Common/StopLog.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace NextStop.Dal.Common;
 
-public class StopLog(int? id, DateTime? time, int? routeStopId, int? tripId)
+public class StopLog(int? id, DateTime? time, int? routeId, string? stopId, int? tripId)
 {
     public int? Id { get; set; } = id;
     public DateTime? Time { get; set; } = time;
-    public int? RouteStopId { get; set; } = routeStopId;
+    public int? RouteId { get; set; } = routeId;
+    public string? StopId { get; set; } = stopId;
     public int? TripId { get; set; } = tripId;
 }
