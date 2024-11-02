22a570cea12127b33b9d0b5aba0f2ef9e6d77716
maxtuec <maxtuec@gmail.com>
2024-11-02 15:11:18 +0100

    add basic dal common setup

---
+++ NextStop/NextStop.Dal.Common/User.cs
@@ -0,0 +1,9 @@
+﻿namespace NextStop.Dal.Common;
+
+public class User(int? userId, string? name, string? password, string? type)
+{
+    public int? UserId { get; set; } = userId;
+    public string? Name { get; set; } = name;
+    public string? Password { get; set; } = password;
+    public string? Type { get; set; } = type;
+}
